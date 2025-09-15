37ae6ce5b88a1f349e1a083e38ddaf0aef9d3006
rajendraphalani <2420030609@klh.edu.in>
2025-09-15 14:34:00 +0530

    Delete pythonch_5 directory

--- pythonch_5/condition.py
+++
@@ -1,8 +0,0 @@
-a = int(input("enter the age :"))
-
-if(a>18):
-    print("u r above the age")
-elif(0>=a):
-    print("it is negatice")
-else:
-    print("u r mot the age")    
--- pythonch_5/pratice3.py
+++
@@ -1,9 +0,0 @@
-a = ["sai","palani", "datta", "kymar"]
-user = input("ente the name : ")
-
-if(user in a):
-    print("it contain lessthan ten chars")
-
-else:
-    print("it is correcti")
-        
